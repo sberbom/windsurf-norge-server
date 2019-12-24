e4e43b48e7724162349dc7c020ffa83af36da5dc
Sigmund Berbom <sberbom@gmail.com>
2019-12-24 13:58:42 +0100

    Secured api

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const Express = require("express");
 const BodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const env = require('./const');
+const {OAuth2Client} = require('google-auth-library');
+
 
 const CONNECTION_URL = `mongodb+srv://${env.user}:${env.password}@sberbom-hh3y2.azure.mongodb.net/test?retryWrites=true&w=majority`;
 const DATABASE_NAME = "windsurfnorge01";
@@ -20,6 +21,22 @@ const profile = require( './profile' );
 app.use( '/profile', profile );
 
 var database;
+
+const client = new OAuth2Client(env.clientID);
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: env.clientID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+}
+
 
 app.listen(3300, () => {
     MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, {useUnifiedTopology: true}, (error, client) => {
@@ -44,22 +61,37 @@ app.get("/spots", (request, response) => {
 });
 
 /**Add spot */
-app.post('/add_spot', (req, res) => {
-    const {name, description, approach, latlng} = req.body;
+
+app.post('/add_spot', async (req, res) => {
+    const {name, description, approach, latlng, token} = req.body;
+    try {
+        await verify(token);
+    }
+    catch(error) {
+        console.error(error);
+        return;
+    }    
     spot = {
         name: name, 
         description: description,
         picture: "",
         latlng: latlng,
-        approach: approach
+        approach: approach,
     };
     spotsdb.insertOne(spot);
     res.send(spot);
 });
 
 /**udate spot */
-app.post("/spot/update", (req, res) => {
-    const {query, update, options} = req.body;
+app.post("/spot/update", async (req, res) => {
+    const {query, update, options, token} = req.body;
+    try {
+        await verify(token);
+    }
+    catch(error) {
+        console.error(error);
+        return;
+    }
     spotsdb.updateOne(query, update, options)
         .then(result => {
             const { matchedCount, modifiedCount } = result;
@@ -71,9 +103,22 @@ app.post("/spot/update", (req, res) => {
         .catch(err => console.error(`Failed to update the item: ${err}`))
 });
 
-app.get("/oneSpot", (request, response) => {
+/**Increment views */
+app.post("/spot/addView", async (req, res) => {
+    const {query, update, options, token} = req.body;
+    spotsdb.updateOne(query, update, options)
+        .then(result => {
+            const { matchedCount, modifiedCount } = result;
+            if (matchedCount && modifiedCount) {
+                console.log(`Successfully updated the item.`)
+                res.send(result)
+            }
+        })
+        .catch(err => console.error(`Failed to update the item: ${err}`))
+});
+
+app.post("/oneSpot", (request, response) => {
     spotsdb.findOne({ name: request.body.name }, (error, result) => {
-        console.log(result)
         if(error) {
             return response.status(500).send(error);
         }
